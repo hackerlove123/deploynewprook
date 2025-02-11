7992819defdb72949d190b2f5dba80d38989300b
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-02-11 08:01:05 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-# Thông tin Telegram bot
-TELEGRAM_BOT_TOKEN="7318225955:AAHbx7sYyLaY-qO3YjEges4ekhmAB2cHEjU"
-TELEGRAM_CHAT_ID="7371969470"
-
-# Lấy đường dẫn thư mục /app
-APP_PATH=$(pwd)
-
-# Tạo thông báo
-MESSAGE="🛠️ Container đã khởi động thành công! 🛠️\n\n📂 Đường dẫn thư mục /app:\n${APP_PATH}"
-
-# Gửi thông báo đến Telegram bot
-curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
-    -d "chat_id=${TELEGRAM_CHAT_ID}" \
-    -d "text=${MESSAGE}" \
-    -d "parse_mode=Markdown"
-
 # Chạy api.js trong nền
 node api.js &
 
