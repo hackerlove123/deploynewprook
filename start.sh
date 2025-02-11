5069264fae3ee6f528dfeafb8b2dbd09bd37e8eb
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-02-11 08:54:14 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,22 @@
 # Chạy api.js trong nền
 node api.js &
 
+# Đợi 3 giây
+sleep 3
+
 # Chạy ok.js trong nền
 node ok.js &
+
+# Đợi 3 giây
+sleep 3
 
 # Chạy prxscan.py trong nền
 python3 prxscan.py -l list.txt &
 
+# Đợi 3 giây
+sleep 3
+
 # Giữ container chạy
-wait
+while true; do
+    sleep 1
+done
